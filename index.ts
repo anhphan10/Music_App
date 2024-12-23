efc9d123f5ad408d1b49243f20f8198422bf33ac
anhphan10 <phananhd09@gmail.com>
2024-12-23 19:28:14 +0700

    createRouteBasic

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("ok");
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.send("Chủ Đề Bài Hát");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
