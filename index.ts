59465ad7ff352e132fb57c15311ea293b8f82d5b
anhphan10 <phananhd09@gmail.com>
2024-12-24 08:02:15 +0700

    Extract the song's theme

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
+import Topic from "./model/topic.model";
 dotenv.config();
 database.connect();
 const app: Express = express();
@@ -9,7 +10,11 @@ const port: number|string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 //Hết
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topic = await Topic.find({
+        deleted: false
+    });
+    console.log(topic);
     res.render("client/pages/topics/index");
 });
 
