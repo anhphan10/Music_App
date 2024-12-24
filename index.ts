025c341afe5b98a862c3e81f752eb9955063deae
anhphan10 <phananhd09@gmail.com>
2024-12-24 07:28:01 +0700

    ConnectDataBase

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+dotenv.config();
+database.connect();
 const app: Express = express();
-const port: number = 3000;
-
+const port: number|string = process.env.PORT || 3000;
 //Để sử dụng pug
 app.set("views", "./views");
 app.set("view engine", "pug");
 //Hết
-
 app.get("/topics", (req: Request, res: Response) => {
     res.render("client/pages/topics/index");
 });
