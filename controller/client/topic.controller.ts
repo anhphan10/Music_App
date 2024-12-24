b9a06832922971a700678c9254ceb6d4e57281d1
anhphan10 <phananhd09@gmail.com>
2024-12-24 11:21:49 +0700

    make a song theme page

--- controller/client/topic.controller.ts
+++ controller/client/topic.controller.ts
@@ -1,11 +1,12 @@
 import Topic from "../../model/topic.model";
-import { Request , Response } from "express";
+import { Request, Response } from "express";
 //[Get]/topics
-export const topics = async (req: Request , res: Response) =>{
+export const topics = async (req: Request, res: Response) => {
     const toipcs = await Topic.find({
         deleted: false
     })
-    res.render("client/pages/topics/index",{
-        pageTitle: "Chủ Đề Bài Hát"
+    res.render("client/pages/topics/index", {
+        pageTitle: "Chủ Đề Bài Hát",
+        topics: toipcs
     });
 }
