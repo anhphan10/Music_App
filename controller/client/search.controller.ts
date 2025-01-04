30a7824aced7839b857be2e629d30f1b9db3b8f5
anhphan10 <phananhd09@gmail.com>
2025-01-04 08:05:29 +0700

    searchSuggest

--- controller/client/search.controller.ts
+++ controller/client/search.controller.ts
@@ -3,8 +3,10 @@ import Song from "../../model/song.model";
 import Singer from "../../model/singer.model";
 import { convertToSlug } from "../../helper/converttoslug";
 
+//[Get]/search/:type
 export const result = async (req: Request, res: Response) => {
     const keyword: string = `${req.query.keyword}`;
+    const type = req.params.type;
     let newSongs = [];
     if (keyword) {
         const keywordRegex = new RegExp(keyword, "i");
@@ -20,13 +22,36 @@ export const result = async (req: Request, res: Response) => {
         for (var item of songs) {
             const infoSinger = await Singer.findOne({ _id: item.singerId }, { deleted: false });
             item["infoSinger"] = infoSinger;
+
+            newSongs.push({
+                id: item.id,
+                title: item.title,
+                avatar: item.avatar,
+                like: item.like,
+                slug: item.slug,
+                infoSinger: {
+                    fullName: infoSinger.fullName
+                }
+            })
         }
-        newSongs = songs
     }
 
-    res.render("client/pages/search/result", {
-        pageTitle: `Kết Quả: ${keyword}`,
-        keyword: keyword,
-        songs: newSongs
-    })
+    switch (type) {
+        case "result":
+            res.render("client/pages/search/result", {
+                pageTitle: `Kết Quả: ${keyword}`,
+                keyword: keyword,
+                songs: newSongs
+            });
+            break;
+        case "suggest":
+            res.json({
+                code: 200,
+                message: "Thành Công!",
+                songs: newSongs
+            })
+            break;
+        default:
+            break;
+    }
 }
--- public/js/script.js
+++ public/js/script.js
@@ -81,3 +81,41 @@ if (listButtonFavorite.length > 0) {
 }
 
 //end
+
+//search Suggest
+const boxSearch = document.querySelector(".box-search");
+if (boxSearch) {
+    const input = boxSearch.querySelector("input[name='keyword']");
+    const boxSuggest = boxSearch.querySelector(".inner-suggest");
+    input.addEventListener("keyup", () => {
+        const keyword = input.value
+
+        const link = `/search/suggest?keyword=${keyword}`;
+        fetch(link)
+            .then(res => res.json())
+            .then(data => {
+                const songs = data.songs;
+                if (songs.length > 0) {
+                    boxSuggest.classList.add("show");
+                    const htmls = songs.map(song => {
+                        return `
+                        <a(class="inner-item" href="/song/detail/${song.slug}")>
+                            <div class="inner-img"><img src="${song.avatar}"/></div>
+                            <div class="inner-info">
+                                <div class="inner-title">${song.title} </div>
+                                <div class="inner-singer"><i class="fa-solid fa-microphone-lines"></i>${song.infoSinger.fullName}
+                            </div>
+                        </a>
+                        `;
+                    });
+
+                    const boxList = boxSuggest.querySelector(".inner-list");
+                    boxList.innerHTML = htmls.join("");
+                } else {
+                    boxSuggest.classList.remove("show");
+                }
+
+            })
+    });
+};
+//end
