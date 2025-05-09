2d8cf0ba4e82893c8f673448e025456d13d05f62
anhphan10 <phananhd09@gmail.com>
2025-05-09 18:17:55 +0700

    loginCSS

--- controller/admin/auth.controller.ts
+++ controller/admin/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Account from "../../model/account.model";
+import { systemConfig } from "../../config/config";
+import md5 from "md5";
 //[Get]/admin/auth/login
 export const login = async (req: Request, res: Response) => {
     res.render("admin/pages/auth/login", {
@@ -8,5 +10,24 @@ export const login = async (req: Request, res: Response) => {
 }
 //[POST]/admin/auth/login
 export const loginPost = async (req: Request, res: Response) => {
+    console.log(req.body);
+    const user = await Account.findOne({ email: req.body.email }, { deleted: false });
+    console.log(user);
 
+    if (!user) {
+        res.redirect(`/${systemConfig.prefixAdmin}/auth/login`)
+        return;
+    }
+
+    if (md5(req.body.password) != user.password) {
+        res.redirect(`/${systemConfig.prefixAdmin}/auth/login`)
+        return;
+    }
+
+    if (user.status == "inactive") {
+        res.redirect(`/${systemConfig.prefixAdmin}/auth/login`)
+        return
+    }
+    res.cookie("token", user.token);
+    res.redirect(`/${systemConfig.prefixAdmin}/dashboard`)
 }
