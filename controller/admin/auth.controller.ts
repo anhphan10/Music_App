59b5b9ca0cdbc83c683ce791c60d40180fc4ae87
anhphan10 <phananhd09@gmail.com>
2025-05-09 15:37:44 +0700

    nd

--- controller/admin/auth.controller.ts
+++ controller/admin/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Request , Response } from "express";
+import { Request, Response } from "express";
+import Account from "../../model/account.model";
 //[Get]/admin/auth/login
-export const login = async (req: Request , res: Response) => {
-    res.render("admin/pages/auth/login",{
-        pageTitle:"Trang Đăng Nhập"
+export const login = async (req: Request, res: Response) => {
+    res.render("admin/pages/auth/login", {
+        pageTitle: "Trang Đăng Nhập"
     })
+}
+//[POST]/admin/auth/login
+export const loginPost = async (req: Request, res: Response) => {
+
 }
--- routes/admin/auth.route.ts
+++ routes/admin/auth.route.ts
@@ -2,5 +2,6 @@ import { Router } from "express";
 const router: Router = Router();
 import * as controller from "../../controller/admin/auth.controller";
 router.get("/login", controller.login);
+router.post("/login", controller.loginPost);
 
 export const authRoutes: Router = router;
